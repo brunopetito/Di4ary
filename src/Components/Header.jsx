e003f86c71f722c10a8c032dbd927cf905da2a36
Bruno Petito <brunopetito97@gmail.com>
2022-07-08 10:21:41 -0300

    mudando algumas coisas do header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import MenuMobile from './helpers/MenuMobile';
 
 import styles from './css/Header.module.css';
 import {
@@ -8,7 +7,7 @@ import {
   Calendar,
   Folders,
   PresentationChart,
-  List,
+  User,
   X,
 } from 'phosphor-react';
 import Foto from '../Assets/foto-perfil.jpg';
@@ -17,7 +16,7 @@ function Header() {
   const [hamburguerToggle, setHamburguerToggle] = React.useState(false);
   return (
     <header
-      className={`bg-zinc-700 flex items-center justify-center h-24 font-poppins  text-sm transition ${
+      className={`bg-zinc-700 flex items-center justify-center h-24 font-poppins  text-sm  ${
         hamburguerToggle ? `${styles.teste}` : null
       }`}
     >
@@ -46,9 +45,73 @@ function Header() {
             }}
           ></button>
           {hamburguerToggle && (
-            <MenuMobile
-              setHamburguerToggle={(setHamburguerToggle, hamburguerToggle)}
-            />
+            <div
+              className={`absolute right-0 w-full top-[96px] z-10 border-t-4  border-[#7ceab7] `}
+            >
+              <div className="py-4 flex flex-col px-4 divide-y  ">
+                <div className=" flex align-center justify-center w-full">
+                  <NavLink
+                    to="/calendario"
+                    className="flex items-center align-left py-3 w-32"
+                    onClick={() => {
+                      setHamburguerToggle(false);
+                    }}
+                  >
+                    <Calendar size={44} />
+                    <p className="text-sm ml-2">Calendário</p>
+                  </NavLink>
+                </div>
+
+                <div className=" flex justify-left w-full justify-center">
+                  <NavLink
+                    to="/turmas"
+                    className="flex  items-center py-3 w-32"
+                    onClick={() => {
+                      setHamburguerToggle(false);
+                    }}
+                  >
+                    <Folders size={44} />
+                    <p className="text-sm ml-2">Turmas</p>
+                  </NavLink>
+                </div>
+                <div className=" flex justify-left w-full justify-center">
+                  <NavLink
+                    to="/notas"
+                    className="flex  items-center py-3 w-32"
+                    onClick={() => {
+                      setHamburguerToggle(false);
+                    }}
+                  >
+                    <Exam size={44} />
+                    <p className="text-sm ml-2">Notas</p>
+                  </NavLink>
+                </div>
+                <div className=" flex justify-left w-full justify-center">
+                  <NavLink
+                    to="/frequencia"
+                    className="flex items-center justify-left py-3 w-32"
+                    onClick={() => {
+                      setHamburguerToggle(false);
+                    }}
+                  >
+                    <PresentationChart size={44} />
+                    <p className="text-sm ml-2">Frequência</p>
+                  </NavLink>
+                </div>
+                <div className=" flex justify-left w-full justify-center">
+                  <NavLink
+                    to="/conta"
+                    className="flex  items-center py-3 w-32"
+                    onClick={() => {
+                      setHamburguerToggle(false);
+                    }}
+                  >
+                    <User size={44} />
+                    <p className="text-sm ml-2">Conta</p>
+                  </NavLink>
+                </div>
+              </div>
+            </div>
           )}
         </div>
 
